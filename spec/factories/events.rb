e4809cdc37c903c0a81e7ebdc36b423f8cefc2f0
Morpheus974 <sabadie.jeremy@gmail.com>
2018-02-08 11:12:37 +0100

    test hello word

---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event do
+    location "MyString"
+    date "2018-02-08 11:07:08"
+    event_id 1
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name "MyString"
+    first_name "MyString"
     last_name "MyString"
     email "MyString"
     password "MyString"
-    confir_password "MyString"
+    confirm_password "MyString"
   end
 end
