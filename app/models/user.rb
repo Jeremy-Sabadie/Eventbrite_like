bc89889357c38d11c47273fc723707936b941476
Morpheus974 <sabadie.jeremy@gmail.com>
2018-02-08 12:06:43 +0100

    generate controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :creations    
+  has_many :events, through: :creations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'events/index'
+
+  get 'events/new'
+
+  get 'events/show'
+
+  get 'events/edit'
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
 	root 'application#home'
 end
